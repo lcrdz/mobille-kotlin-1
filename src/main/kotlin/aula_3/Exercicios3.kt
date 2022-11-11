c3f61039f209a3cb8f4785722b84d2f5e46c0513
Luiz Cardoso <luizcarloscardoso2010@gmail.com>
2022-11-11 19:49:10 -0300

    :sparkles: resolucoes da turma

--- src/main/kotlin/aula_3/Exercicios3.kt
+++ src/main/kotlin/aula_3/Exercicios3.kt
@@ -6,6 +6,8 @@ fun main() {
 //    categoriaNadador()
 //    valorDeZeroADez()
 //    nomeSenhaDiferente()
+//    resolucao1Turma()
+//    resolucao5Turma()
 }
 
 // Ex.1
@@ -23,6 +25,23 @@ fun conversorDeIdade() {
     println("$meses  meses\n$dias dias\n$horas horas\n$minutos minutos")
 
 }
+
+fun resolucao1Turma() {
+    println("Digite sua idade: ")
+    val idade = readln().toInt()
+    println(
+        "Sua idade equivale a: " +
+                "\n${idade.times(QTD_MESES_ANO)} meses "
+                + "\n${idade.times(QTD_DIAS_ANO)} dias" +
+                "\n${idade.times(QTD_HORAS_ANO)} horas " +
+                "\n${idade.times(QTD_MIN_ANO)} minutos"
+    )
+}
+
+const val QTD_MESES_ANO = 12
+const val QTD_DIAS_ANO = 365
+const val QTD_HORAS_ANO = 8760
+const val QTD_MIN_ANO = 525600
 
 // Ex.2
 fun calculoDeIdade() {
@@ -60,7 +79,7 @@ fun valorDeZeroADez() {
     do {
         println("Digite um valor entre 0 e 10:")
         val valor = readln().toIntOrNull()
-    } while ((0..10).contains(valor).not())
+    } while (valor !in 0..10)
 }
 
 // Ex.5
@@ -72,11 +91,27 @@ fun nomeSenhaDiferente() {
         println("Digite sua senha:")
         val senha = readln()
 
-        val entradasInvalidas = usuario.isEmpty() || senha.isEmpty()
+        val entradasInvalidas = usuario.isBlank() || senha.isBlank()
         val entradasIguais = usuario == senha
 
         if (entradasInvalidas) println("Os campos não podem ser vazios")
         if (entradasIguais) println("O usuario não pode ser igual a senha")
     } while (entradasIguais || entradasInvalidas)
 
+}
+
+fun resolucao5Turma() {
+    println("Digite o nome:")
+    var nome = readln().lowercase()
+    println("Digite a senha: ")
+    var senha = readln().lowercase()
+
+    while (nome == senha) {
+        println("Senha inválida, a senha não pode ser igual ao nome.")
+        println("Digite o nome:")
+        nome = readln().lowercase()
+        println("Digite a senha: ")
+        senha = readln().lowercase()
+    }
+    println("OK - Usuário e senha válidos!!")
 }
